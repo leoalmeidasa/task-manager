eda90e17e020c9de0fced62fbf0c3ab09e625153
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-16 17:49:27 -0300

    fix(smtp_settings.rb): fix env heroku name

--- config/initializers/smtp_settings.rb
+++
@@ -1,9 +0,0 @@
-SMTP_SETTINGS = {
-  address: ENV.fetch("SMTP_ADDRESS", "smtp.sendgrid.net"),
-  authentication: ENV.fetch("SMTP_AUTHENTICATION", "plain"),
-  domain: ENV.fetch("SMTP_DOMAIN", "heroku.com"),
-  enable_starttls_auto: true,
-  password: ENV.fetch("SENDGRID_PASSWORD"),
-  port: ENV.fetch("SMTP_PORT", "587"),
-  user_name: ENV.fetch("SENDGRID_USERNAME")
-}.freeze
