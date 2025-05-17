a29bf3511b4890ace4deca16e0b358dae2035f69
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-17 09:48:13 -0300

    fix(devise.rb): add email sender heroku

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
+# app/models/account.rb
 class Account < ApplicationRecord
+  validates :subdomain, presence: true, uniqueness: true
 end
--- db/migrate/20250516192002_create_accounts.rb
+++ db/migrate/20250516192002_create_accounts.rb
@@ -1,10 +1,10 @@
-class CreateAccounts < ActiveRecord::Migration[8.0]
+class CreateAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :accounts do |t|
+      t.string :subdomain, null: false
       t.string :name
-      t.string :subdomain
-
       t.timestamps
     end
+    add_index :accounts, :subdomain, unique: true
   end
 end
