0072b2d706316fe6150f24fa821b2735621dc3dd
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-19 18:53:58 -0300

    add validations user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-
+  validates :email, presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "formato inválido" }
+  validates :password, presence: true,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
   has_many :projects, dependent: :destroy
   has_many :project_members, dependent: :destroy
   has_many :member_projects, through: :project_members, source: :project
