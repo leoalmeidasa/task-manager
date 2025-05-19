06a04cff959d32ac5ba2cfa2008ad124f1270156
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-19 17:13:45 -0300

    erro migrate

--- db/migrate/20250519190304_replace_status_and_priority_with_references.rb
+++ db/migrate/20250519190304_replace_status_and_priority_with_references.rb
@@ -1,26 +1,26 @@
 class ReplaceStatusAndPriorityInTasks < ActiveRecord::Migration[7.1]
   def up
-    # Cria as colunas de referência
-    add_reference :tasks, :status, foreign_key: true
-    add_reference :tasks, :priority, foreign_key: true
-
-    # Popula IDs com base nos valores string existentes
-    Task.reset_column_information
-
-    Task.find_each do |task|
-      status = Status.find_or_create_by!(name: task[:status])
-      priority = Priority.find_or_create_by!(level: task[:priority])
-      task.update_columns(status_id: status.id, priority_id: priority.id)
-    end
-
-    # Remove as colunas antigas
-    remove_column :tasks, :status, :string
-    remove_column :tasks, :priority, :string
-
-    # Garante que status_id e priority_id não aceitem nulo
-    change_column_null :tasks, :status_id, false
-    change_column_null :tasks, :priority_id, false
-  end
+  #   # Cria as colunas de referência
+  #   add_reference :tasks, :status, foreign_key: true
+  #   add_reference :tasks, :priority, foreign_key: true
+  #
+  #   # Popula IDs com base nos valores string existentes
+  #   Task.reset_column_information
+  #
+  #   Task.find_each do |task|
+  #     status = Status.find_or_create_by!(name: task[:status])
+  #     priority = Priority.find_or_create_by!(level: task[:priority])
+  #     task.update_columns(status_id: status.id, priority_id: priority.id)
+  #   end
+  #
+  #   # Remove as colunas antigas
+  #   remove_column :tasks, :status, :string
+  #   remove_column :tasks, :priority, :string
+  #
+  #   # Garante que status_id e priority_id não aceitem nulo
+  #   change_column_null :tasks, :status_id, false
+  #   change_column_null :tasks, :priority_id, false
+  # end
 
   def down
     # Recria as colunas antigas
