fc57b2358f2fc912e153649abc75af14f1dafea0
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-19 14:26:48 -0300

    refactor(application.html;.erb): rfecreat views and implemnts roles for user access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   set_current_tenant_by_subdomain(:account, :subdomain)
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authenticate_user!
+
   def after_sign_in_path_for(resource)
     projects_path if resource.present?
+  end
+
+  private
+
+  def authorize_user_resource(resource)
+    unless resource.user_id == current_user.id
+      flash[:alert] = "Você não tem permissão para acessar este recurso."
+      redirect_to projects_path
+    end
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,9 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
+  before_action :authorize_project, only: %i[show edit update destroy]
 
   def index
-    @projects = Project.all
+    @projects = current_user.projects
   end
 
   def show
@@ -14,7 +15,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.user = User.first # Substitua por lógica de autenticação (ex.: current_user)
+    @project.user = current_user
 
     if @project.save
       redirect_to @project, notice: 'Projeto criado com sucesso.'
@@ -43,6 +44,13 @@ class ProjectsController < ApplicationController
 
   def set_project
     @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Projeto não encontrado."
+    redirect_to projects_path
+  end
+
+  def authorize_project
+    authorize_user_resource(@project)
   end
 
   def project_params
