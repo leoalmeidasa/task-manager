45a47ab430be2310ca6630415626bcf49fed45ca
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-20 11:26:05 -0300

    error cors heroku new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get "projects_members/index"
-  get "projects_members/show"
-  get "projects_members/new"
-  get "projects_members/create"
-  get "projects_members/edit"
-  get "projects_members/update"
-  get "projects_members/destroy"
   resources :projects do
     resources :project_members, only: %i[index show new create destroy]
-    resources :tasks
+    resources :tasks, only: %i[index show new create destroy]
   end
   get "home/index"
   devise_for :users
