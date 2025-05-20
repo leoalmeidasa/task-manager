e78e72bf6069852fe6b6d53bdb7a1d21c65da45a
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-20 10:57:47 -0300

    error cors heroku new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, :set_project
+  before_action :authenticate_user!
+  before_action :set_project
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :authorize_task, only: %i[show edit update destroy]
   before_action :authorize_project_member, only: [:index, :new, :create]
