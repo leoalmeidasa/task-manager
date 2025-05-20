24e4bebd64750cf20430e28cfb19598c04e6717a
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-20 00:22:38 -0300

    rename variavel global

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   before_action :authorize_task, only: %i[show edit update destroy]
 
   def index
-    @project = current_user.projects
+    @task = current_user.tasks
   end
 
   def show
