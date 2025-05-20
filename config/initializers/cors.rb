98826cec2905dec5b863f3981f65349165cbd5e1
Leonardo Almeida <leoalmeidasa@gmail.com>
2025-05-20 00:07:42 -0300

    add gem cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,10 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://task-manager-leo-898e46902197.herokuapp.com/'
-    resource 'https://task-manager-leo-898e46902197.herokuapp.com/',
+    origins 'https://task-manager-leo-898e46902197.herokuapp.com' # Your app's domain
+    resource '*',
              headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true # If using session cookies
   end
 end
